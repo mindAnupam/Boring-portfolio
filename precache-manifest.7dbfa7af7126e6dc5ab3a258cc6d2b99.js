9ae99f1d70816db7b0c2004ce675f688f2cfe600
kanup4m <kanupam500@gmail.com>
2020-08-11 00:30:51 +0530

    Updates

---
+++ precache-manifest.7dbfa7af7126e6dc5ab3a258cc6d2b99.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5acb188b3df255ec31104afc991c2b7",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "c2c3eba3c20c4b6c2831",
+    "url": "/resume/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "c2c3eba3c20c4b6c2831",
+    "url": "/resume/static/js/2.e43790ad.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/resume/static/js/2.e43790ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31d10756f393eb183e4a",
+    "url": "/resume/static/js/main.dcf58b22.chunk.js"
+  },
+  {
+    "revision": "b33888718535e654077b",
+    "url": "/resume/static/js/runtime-main.6cbc6121.js"
+  },
+  {
+    "revision": "51c68ff6a3ed0d0f1a79f8eed8a7f009",
+    "url": "/resume/static/media/Anupam.51c68ff6.png"
+  },
+  {
+    "revision": "d7c656ddbcd8a0b6a487785bd71165f8",
+    "url": "/resume/static/media/br.d7c656dd.svg"
+  },
+  {
+    "revision": "00cc5bdb950c7cd82c90fbe74b26b3cf",
+    "url": "/resume/static/media/c.00cc5bdb.svg"
+  },
+  {
+    "revision": "5be9bde46141498a7cfd4902b92133b1",
+    "url": "/resume/static/media/cplus.5be9bde4.svg"
+  },
+  {
+    "revision": "6971ddc761cc35ba918be0a69dfbcf50",
+    "url": "/resume/static/media/css.6971ddc7.svg"
+  },
+  {
+    "revision": "1190a19ce3cc940741a60137a1e074cb",
+    "url": "/resume/static/media/git.1190a19c.svg"
+  },
+  {
+    "revision": "f0361c5e9bebc1c5647523db385641cf",
+    "url": "/resume/static/media/html.f0361c5e.svg"
+  },
+  {
+    "revision": "2d2e31cc63a96c7e6044f52d30fd21b9",
+    "url": "/resume/static/media/javascript.2d2e31cc.svg"
+  },
+  {
+    "revision": "3c33f5a2cdc20f4ff13ef1bafc2416fb",
+    "url": "/resume/static/media/mongo.3c33f5a2.png"
+  },
+  {
+    "revision": "b5d25ca3c0decaea251b75c69a934e8b",
+    "url": "/resume/static/media/next-js.b5d25ca3.svg"
+  },
+  {
+    "revision": "b337a2fb0b1289e6757e632dc766ff6b",
+    "url": "/resume/static/media/nodejs.b337a2fb.svg"
+  },
+  {
+    "revision": "c3fbd71b12b726e00eec2b59f3c76786",
+    "url": "/resume/static/media/react-2.c3fbd71b.svg"
+  },
+  {
+    "revision": "3837e07e114fb01320b60c0936bd3a13",
+    "url": "/resume/static/media/tl.3837e07e.svg"
+  }
+]);
